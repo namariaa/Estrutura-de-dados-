da83309d9ec0dd21a5704bac5b9d50399fb84838
Ana Maria <144686482+namariaa@users.noreply.github.com>
2025-01-22 18:48:06 +0000

    encaminhamento heap com nó

--- heap/filaPrioridade.java
+++ heap/filaPrioridade.java
@@ -1,8 +1,17 @@
 public class filaPrioridade{
     class Node{
-        private Object key;
+        private int key;
         private Object value;
+        private Node pai;
+        private Node filhoEsquerdo;
+        private Node filhoDireito;
         //Atributos 
+
+        public filaPrioridade(){
+            this.root = null;
+            this.last = null;
+        }
+        //Construtor
 
         public Object getKey(){
             return key;
@@ -20,25 +29,88 @@ public class filaPrioridade{
     }
     //Classe nó 
     private Node root, last; //Armazena o último nó 
+    private int tamanho;
 
     public filaPrioridade(){
         this.root = null;
         this.last = null;
+        this.tamanho = 0;
     }
     //Construtor classe filaPrioridade
 
-    public void insert(Object key, Object value){}
+    public void insert(Object key, Object value){
+        Node no = new Node();
+        no.key = key;
+        no.value = value;
+        no.filhoDireito = null;
+        no.filhoEsquerdo = null;
+        this.tamanho += 1;
+        if (this.root == null){
+            no.pai = null;
+            this.root = no;
+            this.last = no;
+        }
+        else{
+            TheLast();
+            this.last.filhoEsquerdo = no;
+            no.pai = this.last;
+            this.last = no;
+            upHeap();
+        }
+    }
 
-    public void removeMin(){}
+    public void upHeap(){
+        Node pos = this.last;
+        while (pos.pai.key > pos.key){
+            Node antigo = pos.pai;
+            pos.pai = pos;
+            pos = antigo;
+            pos = pos.pai;
+        }
+    }
+
+    public void TheLast(){
+        if (this.last.pai.filhoDireito != this.last){
+            this.last = this.last.pai;
+        }
+        else{
+            Node pos = this.last;
+            while (pos.pai != this.root && pos.pai.filhoEsquerdo != pos){
+                pos = pos.pai;
+            }
+            if (pos.pai.filhoEsquerdo == pos){
+                pos = pos.pai.filhoDireito;
+            }
+            else{
+                pos = pos.pai.filhoEsquerdo;
+            }
+            while (pos.filhoEsquerdo != null){
+                pos = pos.filhoEsquerdo;
+            }
+            this.last = pos;
+        }
+    }
+
+    public void removeMin() throws EFilaPrioridade {
+        if (isEmpty()){
+            throw new EFilaPrioridade("Não há elementos para retirar");
+        }
+        else{
+            this.root = this.last;
+            this.last 
+        }
+    }
 
     public Object min(){
         return this.root.getValue();
     } 
 
-    public int size(){}
+    public int size(){
+        return this.tamanho;   
+    }
 
     public boolean isEmpty(){
-        return last == null;
+        return this.root == null;
     }
     //Métodos de fila de prioridade com heap
 
