bafa4a41ca87996d31acbc07026edda346009f0b
Ana Maria <144686482+namariaa@users.noreply.github.com>
2025-04-22 15:57:19 +0000

    Adiciona base AVL

---
+++ arvoreAVL/ArvoreAVL.java
@@ -0,0 +1,126 @@
+public class ArvoreAVL{
+    //Métodos que eram da binária e adaptei 
+    public class Node{
+        Node pai, filhoDireita, filhoEsquerda;
+        int valor, FB;
+        //Atributos
+
+        public Node getPai(){
+            return pai;
+        }
+        public void setPai(Node noPai){
+            pai = noPai;
+        }
+        //Métodos
+    }
+    //The Node
+    public Node root;
+    //Root
+
+    public ArvoreAVL(){
+        this.root = null;
+    }
+    //Construtor
+
+    public Node theRoot(){
+        return this.root;
+    }
+
+    public void insert(int v){
+        Node novo = new Node();
+        novo.valor = v;
+        novo.FB = 0; //Sempre que insiro um novo ele começa com 0 pois não tem filhos 
+        if (theRoot() == null){
+            novo.filhoDireita = null;
+            novo.filhoEsquerda = null;
+            this.root = novo;
+            novo.pai = null;
+        }
+        else{
+            Node p = this.root;
+            while(p != null){
+                if (p.valor < v){
+                    if (p.filhoDireita == null) break;
+                     p = p.filhoDireita;
+                }
+                else{
+                    if (p.filhoEsquerda == null) break;
+                     p = p.filhoEsquerda;
+                }
+            }
+            novo.setPai(p);
+            if (p.valor > v) p.filhoEsquerda = novo;
+            else p.filhoDireita = novo;
+
+        }
+    }
+
+    public void remove(int v) throws EBinVazio{
+        Node no = buscar(v);
+        if (no == null){
+            throw new EBinVazio("Não existe esse elemento");
+        }
+        else{
+            if (isExternal(no)){ //Quando ele é um nó folha 
+                if (no.pai.filhoDireita == no) no.pai.filhoDireita = null;
+                else no.pai.filhoEsquerda = null;
+            }
+            else if ((no.filhoDireita == null && no.filhoEsquerda != null) || (no.filhoDireita != null && no.filhoEsquerda == null)){ //Só tem um filho
+                Node mudar;
+                if (no.filhoEsquerda != null) mudar = no.filhoEsquerda;
+                else mudar = no.filhoDireita;
+                if (no.pai.filhoDireita == no){
+                    no.pai.filhoDireita = mudar;
+                }
+                else{
+                    no.pai.filhoEsquerda = mudar;
+                }
+                mudar.pai = no.pai;
+            }
+            else{ //Se ele tiver dois filhos 
+                Node p = no.filhoDireita;
+                while (p.filhoEsquerda != null){
+                    p = p.filhoEsquerda;
+                }
+                if (no.filhoDireita.filhoEsquerda == null){ //Tava dando erro no 4 (root) ligado ao 5 e euq uerendo retirar o 5
+                    no.filhoDireita.pai = no;
+                    no.filhoDireita = no.filhoDireita.filhoDireita;
+                }
+                else{
+                    if (p.pai.filhoDireita == p) p.pai.filhoDireita = null;
+                    else p.pai.filhoEsquerda = null;
+                }
+                no.valor = p.valor; 
+            }
+        }
+    }
+
+    public Node buscar(int no){
+        Node p = this.root;
+        while (p != null){
+            if (p.valor == no) break;
+            else{
+                if (p.valor > no) p = p.filhoEsquerda;
+                else p = p.filhoDireita;
+            }
+        }
+        return p;
+    }
+   
+    //Métodos exclusivos da AVL
+    public void atualizarFB(Node no){
+        if (no.pai.filhoDireita == no){ //Se ele for filho da direita 
+            while (no.pai != null){
+                Node pai = no.pai;
+                if (no.pai.filhoDireita)
+                if (pai.filhoDireita == no && filho == 0){
+                    pai.FB -= 1;
+                }
+                else if (pai.filhoEsquerda == no && filho == 1){
+                    pai.FB += 1;
+                    filho = 1;
+                }
+            }
+        }
+    }
+}
