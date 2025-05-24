2d7bf7c0fc8fc9976a98ed89fc1fa564b1475ee9
namariaa <anamariafag18@gmail.com>
2025-05-24 00:48:24 -0300

    Atualiza insert RN

--- arvoreRN/ArvoreRN.java
+++ arvoreRN/ArvoreRN.java
@@ -12,14 +12,19 @@ public class ArvoreRN {
     //The Node
     public Node root;
     //Root
+
+    Node nulo;
+    //Nó nulo
     
     public ArvoreRN(){
-        this.root = null;
+        this.nulo = new Node();
+        this.nulo.cor = negro;
+        this.root = nulo;
     }
     //Construtor
 
     public boolean isExternal(Node no){
-        return (no.filhoDireita == null && no.filhoEsquerda == null);
+        return (no.filhoDireita == nulo && no.filhoEsquerda == nulo);
     }
 
     public Node theRoot(){
@@ -35,96 +40,75 @@ public class ArvoreRN {
 
     public void tratarInsert(Node noInserido){ //Onde vou verificar casos de inserção 
         //Definindo os parente 
-        Node pai =  noInserido.pai;//Como essa verificação eu só faço quando não é null não preciso verificar s eo pai é null
-        Node vozinho = null;
-        Node tio = null;
-        if (pai.pai != null){ //Meu pai não é orfão se ele for meu tio e avô não existem e são considerados negros
-            vozinho = pai.pai;
-            if (vozinho.filhoEsquerda != null){
-                if (vozinho.filhoEsquerda == pai){
-                    if (vozinho.filhoDireita != null) tio = vozinho.filhoDireita;
-                    else tio = null;
-                }
-            }
-            if (vozinho.filhoDireita != null){
-                if (vozinho.filhoDireita == pai){
-                    if (vozinho.filhoEsquerda != null) tio = vozinho.filhoEsquerda;
-                    else tio = null;
-                }
+        Node pai =  noInserido.pai;//Como essa verificação eu só faço quando não é nulo não preciso verificar s eo pai é nulo
+        Node vozinho = pai.pai;
+        Node tio = nulo;
+        if (vozinho.filhoEsquerda != nulo){
+            if (vozinho.filhoEsquerda == pai){
+                if (vozinho.filhoDireita != nulo) tio = vozinho.filhoDireita;
+                else tio = nulo;
             }
         }
+            if (vozinho.filhoDireita != nulo){
+                if (vozinho.filhoDireita == pai){
+                    if (vozinho.filhoEsquerda != nulo) tio = vozinho.filhoEsquerda;
+                    else tio = nulo;
+                }
+            }
 
         //Vamos verirficar os casos agora 
         if (pai.cor == negro){ //Caso 01  
             System.out.println("Tá de boa precisa fazer nada");
         }
-        else if (tio != null){ //Caso 02
-            if ((vozinho == null || vozinho.cor == negro) && tio.cor == rubro && pai.cor == rubro){ //Condição do caso 02
-                if (vozinho != null){
-                    if (vozinho != root) vozinho.cor = rubro;
-                }
-                tio.cor = negro; //Tio não pode ser null se não ele é considerado negro o que não entra no caso 
-                pai.cor = negro;
-            }
+        
+        else if (vozinho.cor == negro && tio.cor == rubro && pai.cor == rubro){ //Caso 02
+            if (vozinho != root && vozinho != nulo) vozinho.cor = rubro;
+            tio.cor = negro; //Tio não pode ser nulo se não ele é considerado negro o que não entra no caso 
+            pai.cor = negro;
         }
 
-        else if ((vozinho == null || vozinho.cor == negro) && (tio == null || tio.cor == negro) && pai.cor == rubro){
-            if (vozinho != null){ 
-                if (vozinho.filhoEsquerda != null){ 
-                    if (vozinho.filhoEsquerda == pai){
-                        if (pai.filhoEsquerda != null){ //Verificar se é o 3A
-                            if (pai.filhoEsquerda == noInserido) rotacaoDireita(noInserido);
-                        }
-                        if (pai.filhoDireita != null){ //Verifica se é 3C
-                            if (pai.filhoDireita == noInserido) rotacaoDuplaEsquerda(noInserido);
-                        }
-                    }
-                }
-
-                if (vozinho.filhoDireita != null){ //Verifica se é 3B
-                    if (vozinho.filhoDireita == pai){
-                        if (pai.filhoDireita != null){
-                            if (pai.filhoDireita == noInserido) rotacaoEsquerda(noInserido);
-                        }
-                        if (pai.filhoEsquerda != null){ //Verifica  se é 3D
-                            if (pai.filhoEsquerda == noInserido) rotacaoDuplaDireita(noInserido);
-                        }
-                    }
-                }
+        else if (vozinho.cor == negro && tio.cor == negro && pai.cor == rubro){
+            System.out.println(pai.filhoEsquerda == noInserido);
+            if (vozinho.filhoEsquerda == pai){
+                    if (pai.filhoEsquerda == noInserido) rotacaoDireita(vozinho); //Verificar se é o 3A
+                    if (pai.filhoDireita == noInserido) rotacaoDuplaDireita(vozinho); //Verifica se é 3C
             }
-
+            else if (vozinho.filhoDireita == pai){ 
+                if (pai.filhoDireita == noInserido) rotacaoEsquerda(vozinho); //Verifica se é 3B
+                if (pai.filhoEsquerda == noInserido) rotacaoDuplaEsquerda(vozinho); //Verifica  se é 3D        
+            }
         }
     }
 
     public void tratarRemove(Node noInserido){
-        Node pai =  noInserido.pai;//Como essa verificação eu só faço quando não é null não preciso verificar s eo pai é null
-        Node vozinho = null;
-        Node irmao, sobrinhoDireito, sobrinhoEsquerdo = null;
-        if (pai.filhoEsquerda != null){
+        Node pai =  noInserido.pai;//Como essa verificação eu só faço quando não é nulo não preciso verificar s eo pai é nulo
+        Node vozinho = nulo;
+        Node irmao, sobrinhoDireito, sobrinhoEsquerdo = nulo;
+        if (pai.filhoEsquerda != nulo){
             if (pai.filhoEsquerda == noInserido){ //Se sou filho esquerdo 
-                if (pai.pai != null){ //Meu pai não é orfão se ele for meu irmao e avô não existem e são considerados negros
+                if (pai.pai != nulo){ //Meu pai não é orfão se ele for meu irmao e avô não existem e são considerados negros
                     vozinho = pai.pai;
-                    if (vozinho.filhoEsquerda != null){
+                    if (vozinho.filhoEsquerda != nulo){
                         if (vozinho.filhoEsquerda == pai){
-                            if (vozinho.filhoDireita != null) irmao = vozinho.filhoDireita;
-                            else irmao = null;
+                            if (vozinho.filhoDireita != nulo) irmao = vozinho.filhoDireita;
+                            else irmao = nulo;
                         }
                     }
-                    if (vozinho.filhoDireita != null){
+                    if (vozinho.filhoDireita != nulo){
                         if (vozinho.filhoDireita == pai){
-                            if (vozinho.filhoEsquerda != null) irmao = vozinho.filhoEsquerda;
-                            else irmao = null;
+                            if (vozinho.filhoEsquerda != nulo) irmao = vozinho.filhoEsquerda;
+                            else irmao = nulo;
                         }
                     }
                 }
             }
         }
-        if (pai.filhoDireita != null){
+        if (pai.filhoDireita != nulo){
             if (pai.filhoDireita == noInserido){//Se sou filho direito aqui vai cobrir casos espelhados 
-                if (pai.filhoEsquerda != null){
+                if (pai.filhoEsquerda != nulo){
                     irmao = pai.filhoEsquerda;
-                    if (irmao.filhoEsquerda != null) sobrinhoDireito = irmao.filhoEsquerda;
-                    if (irmao.filhoDireita != null) sobrinhoEsquerdo = irmao.filhoDireita;
+                    if (irmao.filhoEsquerda != nulo) sobrinhoDireito = irmao.filhoEsquerda;
+                    if (irmao.filhoDireita != nulo) sobrinhoEsquerdo = irmao.filhoDireita;
                 }
             }
         }
@@ -133,30 +117,32 @@ public class ArvoreRN {
     public void insert(int v){
         Node novo = new Node();
         novo.valor = v;
-        if (theRoot() == null){
-            novo.filhoDireita = null;
-            novo.filhoEsquerda = null;
+        if (theRoot() == nulo){
+            novo.filhoDireita = nulo;
+            novo.filhoEsquerda = nulo;
             this.root = novo;
-            novo.pai = null;
+            novo.pai = nulo;
             novo.cor = negro; //Todo nó novo é 
         }
         else{
             Node p = this.root;
-            while(p != null){
+            while(p != nulo){
                 if (p.valor < v){
-                    if (p.filhoDireita == null) break;
+                    if (p.filhoDireita == nulo) break;
                     p = p.filhoDireita;
                 }
                 else{
-                    if (p.filhoEsquerda == null) break;
+                    if (p.filhoEsquerda == nulo) break;
                     p = p.filhoEsquerda;
                 }
             }
-            novo.pai = p;
             novo.cor = rubro;
+            novo.pai = p;
             if (p.valor > v) p.filhoEsquerda = novo;
             else p.filhoDireita = novo;
-
+            novo.filhoDireita = nulo;
+            novo.filhoEsquerda = nulo;
+            
             //Agora vamos verificar os casos 
             rebalancear(novo, "insert");
             System.out.println("inserido: " + novo.valor + " " + novo.cor + " Pai: " + novo.pai.valor + " " + novo.pai.cor);
@@ -172,8 +158,8 @@ public class ArvoreRN {
         novoRoot.filhoEsquerda = no;
         no.pai = novoRoot;
         novoRoot.pai = antigoPaiRoot; //O pai do antigo root vai ser pai do root atual
-        if (antigoPaiRoot != null){ //Se ele não era o root ele tinha um pai
-            if (antigoPaiRoot.filhoEsquerda != null){
+        if (antigoPaiRoot != nulo){ //Se ele não era o root ele tinha um pai
+            if (antigoPaiRoot.filhoEsquerda != nulo){
                 if (antigoPaiRoot.filhoEsquerda == no) antigoPaiRoot.filhoEsquerda = novoRoot;
                 else antigoPaiRoot.filhoDireita = novoRoot;
             }
@@ -182,7 +168,7 @@ public class ArvoreRN {
             }
         }
         no.filhoDireita = antigoEsquerdo;
-        if (antigoEsquerdo != null) antigoEsquerdo.pai = no;
+        if (antigoEsquerdo != nulo) antigoEsquerdo.pai = no;
         no.cor = novoRoot.cor;
         novoRoot.cor = antigaCorRoot;
         if (no == theRoot()) root = novoRoot; //Se o nó era o root eu tenho que definir e não uma subarvore eu tenho que definir o novo root
@@ -196,8 +182,8 @@ public class ArvoreRN {
         novoRoot.filhoDireita = no;
         no.pai = novoRoot;
         novoRoot.pai = antigoPaiRoot;
-        if (antigoPaiRoot != null){ //Se ele não era o root ele tinha um pai
-            if (antigoPaiRoot.filhoEsquerda != null){
+        if (antigoPaiRoot != nulo){ //Se ele não era o root ele tinha um pai
+            if (antigoPaiRoot.filhoEsquerda != nulo){
                 if (antigoPaiRoot.filhoEsquerda == no) antigoPaiRoot.filhoEsquerda = novoRoot;
                 else antigoPaiRoot.filhoDireita = novoRoot;
             }
@@ -206,7 +192,7 @@ public class ArvoreRN {
             }
         }
         no.filhoEsquerda = antigoDireito;
-        if (antigoDireito != null) antigoDireito.pai = no;
+        if (antigoDireito != nulo) antigoDireito.pai = no;
         no.cor = novoRoot.cor;
         novoRoot.cor = antigaCorRoot;
         if (no == theRoot()) root = novoRoot; //Se o nó era o root eu tenho que definir e não uma subarvore eu tenho que definir o novo root
@@ -218,6 +204,7 @@ public class ArvoreRN {
         String antigaCorRoot = no.cor;
         Node novoRoot = no.filhoDireita;
         rotacaoEsquerda(no);
+        System.out.println("no: " + root.valor);
         no.cor = novoRoot.cor;
         novoRoot.cor = antigaCorRoot;
     }
@@ -234,44 +221,44 @@ public class ArvoreRN {
 
     public void remove(int v) throws EArvoreRN{
         Node no = buscar(v);
-        if (no == null){
+        if (no == nulo){
             throw new EArvoreRN("Não existe esse elemento");
         }
         else{
             if (isExternal(no)){ //Quando ele é um nó folha 
-                if (no.pai.filhoDireita == no) no.pai.filhoDireita = null;
-                else no.pai.filhoEsquerda = null;
+                if (no.pai.filhoDireita == no) no.pai.filhoDireita = nulo;
+                else no.pai.filhoEsquerda = nulo;
             }
-            else if ((no.filhoDireita == null && no.filhoEsquerda != null) || (no.filhoDireita != null && no.filhoEsquerda == null)){ //Só tem um filho
+            else if ((no.filhoDireita == nulo && no.filhoEsquerda != nulo) || (no.filhoDireita != nulo && no.filhoEsquerda == nulo)){ //Só tem um filho
                 Node mudar;
-                if (no.filhoEsquerda != null) mudar = no.filhoEsquerda;
+                if (no.filhoEsquerda != nulo) mudar = no.filhoEsquerda;
                 else mudar = no.filhoDireita;
-                if (no.pai != null){
-                    if (no.pai.filhoDireita != null){
+                if (no.pai != nulo){
+                    if (no.pai.filhoDireita != nulo){
                         if (no.pai.filhoDireita == no){
                             no.pai.filhoDireita = mudar;
                         }
                     }
-                    if (no.pai.filhoDireita == null) no.pai.filhoEsquerda = mudar;
+                    if (no.pai.filhoDireita == nulo) no.pai.filhoEsquerda = mudar;
                 }
                 
                 mudar.pai = no.pai;
-                if (mudar.pai == null) root = mudar; //Se ele não tem pai é root isso se dá quando o que eu removo é root
-                no = mudar; //Perigoso, mudei pois quando for chamar atualizar ele não reconhece no e dá null pois as alterações estão em mudar 
+                if (mudar.pai == nulo) root = mudar; //Se ele não tem pai é root isso se dá quando o que eu removo é root
+                no = mudar; //Perigoso, mudei pois quando for chamar atualizar ele não reconhece no e dá nulo pois as alterações estão em mudar 
             }
             else{ //Se ele tiver dois filhos 
                 Node p = no.filhoDireita;
-                while (p.filhoEsquerda != null){
+                while (p.filhoEsquerda != nulo){
                     p = p.filhoEsquerda;
                 }
-                if (no.filhoDireita.filhoEsquerda == null){ 
-                    no.filhoDireita = null;
+                if (no.filhoDireita.filhoEsquerda == nulo){ 
+                    no.filhoDireita = nulo;
                 } 
                 else {
-                    p.pai.filhoEsquerda = null;
+                    p.pai.filhoEsquerda = nulo;
                 }
                 no.valor = p.valor; 
-                if (no.pai == null) root = no; 
+                if (no.pai == nulo) root = no; 
                 no = p; 
             }
         }
@@ -279,7 +266,7 @@ public class ArvoreRN {
 
     public Node buscar(int no){
         Node p = this.root;
-        while (p != null){
+        while (p != nulo){
             if (p.valor == no) break;
             else{
                 if (p.valor > no) p = p.filhoEsquerda;
--- arvoreRN/teste.java
+++ arvoreRN/teste.java
@@ -1,10 +1,11 @@
 public class teste {
     public static void main(String [] args){
         ArvoreRN lista = new ArvoreRN();
-        lista.insert(4);
         lista.insert(3);
         lista.insert(2);
-
+        lista.insert(6);
+        lista.insert(4);
+        lista.insert(5);
         System.out.println(lista.root.valor + " " + lista.root.cor);
     }
 }
